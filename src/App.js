8d7fecd239df5e6584f9f7eb88bf1a65e2d40e91
Anuj Singla <asingla@redhat.com>
2022-09-14 10:59:12 +0530

    code changes .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing Github Pages Edit <code>src/App.js</code> and save to reload.
+          Testing Github Pages (After changes) Edit <code>src/App.js</code> and
+          save to reload.
         </p>
         <a
           className="App-link"
